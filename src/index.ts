9421eb047301459e01820a23bac49ebd9786be99
Peter Timpane <petert2022@headroyce.org>
2021-11-16 17:26:33 -0800

    Changed Grid from a class to a function, drawGrid, which returns the grid. Grid cells are now individually colorable, takes the logical representation of the board as an argument

--- src/index.ts
+++ src/index.ts
@@ -3,31 +3,30 @@ import { Graphics } from '@pixi/graphics';
 import { Application } from 'pixi.js';
 //import { makeRect } from './tetromino';
 
+function drawGrid(rows:number, columns: number, length:number, colorGrid: number[][]) : Graphics{
 
+	const pieceColors: number[] = [0xd1d1d1,//0 = open
+		0x2BD4FF,//1 = I
+		0x001EFF,//2 = J
+		0xFFB10D,//3 = L
+		0xE9FF00,//4 = O 
+		0xE612FF,//5 = T 
+		0x1BC000,//6 = S 
+		0xFF2B09];//7 = Z
 
-class Grid {
-	cols: number;
-	rows: number;
-	length: number;
-	constructor(c:number,r:number,l:number) {
-		this.cols = c;
-		this.rows = r;
-		this.length = l;
-	}
+	let ret: Graphics = new Graphics();
 
-	public draw(): Graphics {
-		let ret: Graphics = new Graphics();
 		
-		for (let c = 0; c < this.cols; c++) {
-			for (let r = 0; r < this.rows; r++) {
-				ret.lineStyle(3,0xe1e1e1);
-				ret.drawRect(c*this.length, r*this.length, this.length,this.length);
-			}
+	for (let c = 0; c < columns; c++) {
+		for (let r = 0; r < rows; r++) {
+			ret.lineStyle(3,0xe1e1e1);
+			ret.beginFill(pieceColors[colorGrid[r][c]])
+			ret.drawRect(c*length, r*length, length,length);
+			ret.endFill();
 		}
-
-		return ret;
 	}
 
+	return ret;
 }
 
 const app = new Application({
@@ -40,18 +39,34 @@ const app = new Application({
 });
 
 
+
 const conty: Container = new Container();
 conty.x = window.innerWidth/2 - (10*window.innerHeight)/44;
 conty.y = 30;
 
+let colors : number[][] = //Representation for the board
+[[0,0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,5,0,0,0,0,0],
+ [0,0,0,5,5,5,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0,0], 
+ [0,0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,1,1,1,1,0]];
 
-//const block = makeRect(100,100,50,50);
-
-const grid: Grid = new Grid(10,20,(window.innerHeight)/22);
-
-
-
-conty.addChild(grid.draw());
+conty.addChild(drawGrid(20,10,(window.innerHeight)/22,colors));
 
 app.stage.addChild(conty);
 
